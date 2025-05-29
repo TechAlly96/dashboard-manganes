9bf1492715439e22ccd4874915560111ca4063d6
TechAlly96 <techally96@gmail.com>
2025-05-29 09:40:49 -0300

    corrigido uso de porta fixa para variável de ambiente PORT

--- dashboard_manganes_3d.py
+++ dashboard_manganes_3d.py
@@ -176,7 +176,7 @@ def atualizar_cutoff(corte):
 
 server = app.server  # para Railway identificar o servidor
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
 
